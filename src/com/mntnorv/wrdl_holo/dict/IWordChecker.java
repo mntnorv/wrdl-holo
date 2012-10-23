5a9dd5d2b15d003488be2bed349ac7287f56b584
Mantas Norvaiša <mntnorv@gmail.com>
2012-10-23 20:28:58 +0300

    Added IWordChecker interface

---
+++ src/com/mntnorv/wrdl_holo/dict/IWordChecker.java
@@ -0,0 +1,46 @@
+package com.mntnorv.wrdl_holo.dict;
+
+public interface IWordChecker {
+	public Result checkWord (String pWord);
+	public int getWordScore (String pWord);
+	
+	public class Result {
+		private int score;
+		private byte state;
+		
+		public static final byte EMPTY   = (byte) 0;
+		public static final byte GOOD    = (byte) 1;
+		public static final byte BAD     = (byte) 2;
+		public static final byte GUESSED = (byte) 4;
+		
+		public Result () {
+			state = EMPTY;
+			score = 0;
+		}
+		
+		public Result (byte pState, int pScore) {
+			state = pState;
+			score = pScore;
+		}
+		
+		public boolean isGood () {
+			return (state | GOOD) == state;
+		}
+		
+		public boolean isBad () {
+			return (state | BAD) == state;
+		}
+		
+		public boolean isGuessed () {
+			return (state | GUESSED) == state;
+		}
+		
+		public boolean isEmpty () {
+			return state == EMPTY;
+		}
+		
+		public int getScore () {
+			return score;
+		}
+	}
+}
