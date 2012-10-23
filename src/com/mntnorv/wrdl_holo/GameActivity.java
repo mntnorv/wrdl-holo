c6955ca27f84292ff1ac53ffefe86a18d4e3ff98
Mantas Norvaiša <mntnorv@gmail.com>
2012-10-23 20:29:49 +0300

    Started using dictionary

--- src/com/mntnorv/wrdl_holo/GameActivity.java
+++ src/com/mntnorv/wrdl_holo/GameActivity.java
@@ -1,10 +1,16 @@
 package com.mntnorv.wrdl_holo;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.EditText;
 
+import com.mntnorv.wrdl_holo.dict.Dictionary;
+import com.mntnorv.wrdl_holo.dict.LetterGrid;
 import com.mntnorv.wrdl_holo.views.TileGridView;
 
 public class GameActivity extends Activity {
@@ -14,6 +20,8 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_game);
+        
+        final List<String> words = new ArrayList<String>();
         
         final TileGridView grid = (TileGridView)findViewById(R.id.mainTileGrid);
         final EditText wordField = (EditText)findViewById(R.id.currentWordField);
@@ -28,9 +36,20 @@ public class GameActivity extends Activity {
         grid.setOnWordChangeListener(new TileGridView.OnWordChangeListener() {	
 			@Override
 			public void onWordChange(String word) {
-				wordField.setText(word);
+				wordField.setText(word.toUpperCase());
 			}
 		});
+        
+        Dictionary dict = null;
+        
+		try {
+			dict = new Dictionary(getAssets().open("dict.hex"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+        LetterGrid lGrid = new LetterGrid(letters, 4, 4);
+        words.addAll(lGrid.getWordsInGrid(dict));
     }
 
     @Override
