89a0133a34293160c212f9fbcec0110e5109b271
Mantas Norvaiša <mntnorv@gmail.com>
2013-02-08 12:56:23 +0200

    Updated FlatProgressBarView class
    Added required fields and an init method

--- src/com/mntnorv/wrdl_holo/views/FlatProgressBarView.java
+++ src/com/mntnorv/wrdl_holo/views/FlatProgressBarView.java
@@ -1,20 +1,64 @@
 package com.mntnorv.wrdl_holo.views;
 
 import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class FlatProgressBarView extends View {
+	
+	private float width;
+	private float height;
+	
+	private float progress;
+	private float maxProgress;
+	
+	private Paint backgroundPaint;
+	private Paint foregroundPaint;
+	private Paint textPaint;
+	
+	private Rect textBounds;
+	
+	private String progressText;
 
 	public FlatProgressBarView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		initProgressBar();
 	}
 
 	public FlatProgressBarView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		initProgressBar();
 	}
 
-	
+	private void initProgressBar() {
+		width = 120;
+		height = 30;
+		progress = 0;
+		maxProgress = 100;
+		
+		progressText = "0";
+		
+		// Default background paint
+		backgroundPaint = new Paint();
+		backgroundPaint.setColor(0x00000000);
+		
+		// Default foreground paint
+		foregroundPaint = new Paint();
+		foregroundPaint.setColor(0xFF000000);
+		
+		// Default text paint
+		textPaint = new Paint();
+		textPaint.setAntiAlias(true);
+		textPaint.setSubpixelText(true);
+		textPaint.setColor(0xFF000000);
+		textPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+		textPaint.setTextAlign(Align.CENTER);
+		
+		// Get text bounds
+		textBounds = new Rect();
+		textPaint.getTextBounds(progressText, 0, progressText.length(), textBounds);
+	}
 }
