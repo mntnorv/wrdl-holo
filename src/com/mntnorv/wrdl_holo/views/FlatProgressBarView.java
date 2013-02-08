499ae046053a30d621dc932bc34f4a620561d407
Mantas Norvaiša <mntnorv@gmail.com>
2013-02-08 12:29:17 +0200

    Added FlatProgressBarView stub

---
+++ src/com/mntnorv/wrdl_holo/views/FlatProgressBarView.java
@@ -0,0 +1,20 @@
+package com.mntnorv.wrdl_holo.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FlatProgressBarView extends View {
+
+	public FlatProgressBarView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FlatProgressBarView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
