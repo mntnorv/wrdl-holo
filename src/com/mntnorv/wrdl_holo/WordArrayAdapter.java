d0cbef9233f76a6e96034da0630a76f83d96f920
Mantas Norvaiša <mntnorv@gmail.com>
2013-02-10 17:09:35 +0200

    Updated WordArrayAdapter to use ScoreCounter

--- src/com/mntnorv/wrdl_holo/WordArrayAdapter.java
+++ src/com/mntnorv/wrdl_holo/WordArrayAdapter.java
@@ -2,17 +2,17 @@ package com.mntnorv.wrdl_holo;
 
 import java.util.List;
 
-import com.mntnorv.wrdl_holo.dict.WordChecker;
-
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.mntnorv.wrdl_holo.dict.ScoreCounter;
+
 public class WordArrayAdapter extends ArrayAdapter<String> {
 	private int scoreId;
-	private WordChecker wordChecker;
+	private ScoreCounter scoreCounter;
 	
 	public WordArrayAdapter (Context context, int layoutId, int wordId, int scoreId, List<String> itemList) {
 		super(context, layoutId, wordId, itemList);
@@ -20,17 +20,17 @@ public class WordArrayAdapter extends ArrayAdapter<String> {
 		this.scoreId = scoreId;
 	}
 	
-	public void setWordChecker(WordChecker checker) {
-		this.wordChecker = checker;
+	public void setScoreCounter(ScoreCounter counter) {
+		this.scoreCounter = counter;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View returnedView = super.getView(position, convertView, parent);
 		
-		if (wordChecker != null) {
+		if (scoreCounter != null) {
 			TextView scoreField = (TextView)returnedView.findViewById(scoreId);
-			String test = "+" + Integer.toString(wordChecker.getWordScore(super.getItem(position)));
+			String test = "+" + Integer.toString(scoreCounter.getWordScore(super.getItem(position)));
 			scoreField.setText(test);
 			returnedView.invalidate();
 		}
