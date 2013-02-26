525f71ba5f3e311d1ae2c2d5f1b08110bc659163
Mantas Norvaiša <mntnorv@gmail.com>
2013-02-26 16:16:50 +0200

    Added simple main menu activity

---
+++ src/com/mntnorv/wrdl_holo/MainMenuAdapter.java
@@ -0,0 +1,74 @@
+package com.mntnorv.wrdl_holo;
+
+import java.util.List;
+
+import com.mntnorv.wrdl_holo.views.TileGridView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MainMenuAdapter extends BaseAdapter {
+
+	private static final int ITEM_LAYOUT_ID = R.layout.game_state_item;
+	private static final int GRID_ID = R.id.gameGridView;
+	private static final int NAME_ID = R.id.itemName;
+	
+	private List<GameState> gameStateList;
+	private Context context;
+	private LayoutInflater inflater;
+	
+	public MainMenuAdapter(Context context, List<GameState> objects) {
+		super();
+		this.context = context;
+		this.gameStateList = objects;
+		
+		inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	@Override
+	public int getCount() {
+		return gameStateList.size();
+	}
+
+	@Override
+	public Object getItem(int index) {
+		return gameStateList.get(index);
+	}
+
+	@Override
+	public long getItemId(int index) {
+		return index;
+	}
+
+	@Override
+	public View getView(int index, View convertView, ViewGroup parentView) {
+		convertView = inflater.inflate(ITEM_LAYOUT_ID, parentView, false);
+		TileGridView grid = (TileGridView)convertView.findViewById(GRID_ID);
+		TextView name = (TextView)convertView.findViewById(NAME_ID);
+		
+		grid.create(gameStateList.get(index).getColumns(), gameStateList.get(index).getRows());
+		grid.setLetters(gameStateList.get(index).getLetterArray());
+		name.setText(Integer.toString(gameStateList.get(index).getGuessedWordCount()));
+		
+		return convertView;
+	}
+
+	@Override
+	public boolean hasStableIds(){
+		return true;
+	}
+
+	@Override
+	public int getItemViewType(int pos){
+		return IGNORE_ITEM_VIEW_TYPE;
+	}
+
+	@Override
+	public int getViewTypeCount(){
+		return 1;
+	}
+}
---
+++ src/com/mntnorv/wrdl_holo/MenuActivity.java
@@ -0,0 +1,57 @@
+package com.mntnorv.wrdl_holo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mntnorv.wrdl_holo.dict.Dictionary;
+import com.mntnorv.wrdl_holo.util.StringGenerator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+
+public class MenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+		
+		// Load dictionary
+        Dictionary dict = null;
+		try {
+			dict = new Dictionary(getAssets().open("dict.hex"));
+		} catch (IOException e) {
+			Log.e("dictionary", "Error laoding dictionary from \"dict.hex\"");
+		}
+        
+        // Create a game
+		GameState gameState;
+        gameState = new GameState(4, 4, StringGenerator.randomString(4 * 4));
+        gameState.findAllWords(dict);
+        List<GameState> states = new ArrayList<GameState>();
+        states.add(gameState);
+		
+		ListView menuListView = (ListView)findViewById(R.id.menuListView);
+		MainMenuAdapter menuAdapter = new MainMenuAdapter(this, states);
+		menuListView.setAdapter(menuAdapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_menu, menu);
+		return true;
+	}
+
+	public void startNewGame(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, GameActivity.class);
+		startActivity(intent);
+	}
+}
